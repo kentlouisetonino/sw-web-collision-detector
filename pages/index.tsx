c18861f4e0ddbd85b7c9b361d30b204da6d835a7
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-12-28 13:20:10 +0800

    do not support mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import GameBox from '../libs/design/GameBox'
 import NextHead from '../libs/design/NextHead'
 
 export default function Home() {
+  const [browserWidth, setBrowserWidth] = useState(0)
+
+  useEffect(() => {
+    setBrowserWidth(window.innerWidth)
+  }, [])
+
   return (
     <Fragment>
-      <NextHead title="Collision Detector" />
-      <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
-      <GameBox />
+      {browserWidth < 600 ? (
+        <h1 className="text-base my-10">
+          Sorry this app does not support mobile view less than 600 pixel.
+        </h1>
+      ) : (
+        <Fragment>
+          <NextHead title="Collision Detector" />
+          <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
+          <GameBox />
+        </Fragment>
+      )}
     </Fragment>
   )
 }
