94e5f5c47499e20bdfbe030e4526c9fefadf623b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-12-28 13:37:36 +0800

    modify meta content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Fragment } from 'react'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <link
+          rel="icon"
+          href="/static/images/logo/favicon.png"
+          type="image/png"
+          sizes="16x16"
+        />
+        <meta name="viewport" content="width=600" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment } from 'react'
 import GameBox from '../libs/design/GameBox'
 import NextHead from '../libs/design/NextHead'
 
 export default function Home() {
-  const [browserWidth, setBrowserWidth] = useState(0)
-
-  useEffect(() => {
-    setBrowserWidth(window.innerWidth)
-
-    function handleResize() {
-      setBrowserWidth(window.innerWidth)
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  })
-
-  if (browserWidth < 600) {
-    return (
-      <h1 className="text-base my-10">
-        Sorry this app does not support mobile view less than 600 pixel.
-      </h1>
-    )
-  }
-
   return (
     <Fragment>
       <NextHead title="Collision Detector" />
