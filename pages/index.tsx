6c5f935ec9b44c6c1d701a9c1f59a1906a9d3559
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-12-28 13:32:19 +0800

    add note in mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,31 @@ export default function Home() {
 
   useEffect(() => {
     setBrowserWidth(window.innerWidth)
-  }, [])
+
+    function handleResize() {
+      setBrowserWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  if (browserWidth < 600) {
+    return (
+      <h1 className="text-base my-10">
+        Sorry this app does not support mobile view less than 600 pixel.
+      </h1>
+    )
+  }
 
   return (
     <Fragment>
-      {browserWidth < 600 ? (
-        <h1 className="text-base my-10">
-          Sorry this app does not support mobile view less than 600 pixel.
-        </h1>
-      ) : (
-        <Fragment>
-          <NextHead title="Collision Detector" />
-          <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
-          <GameBox />
-        </Fragment>
-      )}
+      <NextHead title="Collision Detector" />
+      <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
+      <GameBox />
     </Fragment>
   )
 }
