4068d4880bae25b16adfd65030098e621bd772ed
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-12-28 14:08:01 +0800

    add width detector

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import GameBox from '../libs/design/GameBox'
 
 export default function Home() {
+  const [browserWidth, setBrowserWidth] = useState(0)
+
+  function handleRezie() {
+    setBrowserWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleRezie)
+      handleRezie()
+      return () => window.removeEventListener('resize', handleRezie)
+    }
+  }, [])
+
+  if (browserWidth < 600) {
+    return (
+      <h1 className="text-base mx-20 my-96">
+        Sorry this app does not support mobile devices with less 600 pixel
+        width. If possible toggle desktop site option in your browser.
+      </h1>
+    )
+  }
+
   return (
     <Fragment>
       <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
