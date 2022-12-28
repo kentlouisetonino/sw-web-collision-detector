caad0503ead6ce9ae597677c5f6f43571eff72c7
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-12-28 09:57:29 +0800

    change title

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,7 @@ export default function Home() {
 
   return (
     <Fragment>
-      <h1 className="text-4xl font-bold my-10">Coin Collector</h1>
+      <h1 className="text-4xl font-bold my-10">Collision Detector</h1>
       <canvas
         ref={canvasRef}
         id="canvas"
